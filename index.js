c569524f2e0adb9c9d7a04cfd344b9b297e9a4ce
Permutator <permutatorem@gmail.com>
2015-11-15 18:20:29 -0800

    Made it actually work

--- index.js
+++ index.js
@@ -5,22 +5,20 @@ module.exports = function rollupStream(options) {
   var stream = new Readable();
   stream._read = function() {  };
   
-  if(options && options.sourceMap) {
-    options.sourceMap = 'inline';
-  }
-  
   rollup(options).then(function(bundle) {
-    bundle = bundle.generate();
+    bundle = bundle.generate(options);
     var code = bundle.code, map = bundle.map;
     
-    if(options.sourceMap) {
-      code += '\n//# sourceMappingURL=' + map.toUrl();
-    }
-    
     stream.push(code);
+    if(options.sourceMap) {
+      stream.push('\n//# sourceMappingURL=');
+      stream.push(map.toUrl());
+    }
     stream.push(null);
-  }, function(reason) {
-    stream.emit('error', reason);
+  }).catch(function(reason) {
+    setImmediate(function() {
+      stream.emit('error', reason);
+    });
   });
   
   return stream;
