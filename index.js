b9053fffee565e7e0bea8f04f9c9c8594fba0b58
Permutator <permutatorem@gmail.com>
2016-06-30 10:13:16 -0700

    Made it import options

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
 var Readable = require('stream').Readable;
+var path = require('path');
 
 module.exports = function rollupStream(options) {
   var stream = new Readable();
   stream._read = function() {  };
   
-  var rollup = (options && options.rollup) || require('rollup');
-  if(options && 'rollup' in options) {
-    delete options.rollup;
+  if(typeof options !== 'string') {
+    options = Promise.resolve(options);
+  } else {
+    options = path.resolve(options);
+    options = require('rollup').rollup({
+      entry: options,
+      onwarn: function(message) {
+        if(!/Treating .+ as external dependency/.test(message)) {
+          console.error(message);
+        }
+      }
+    }).then(function(bundle) {
+      var code = bundle.generate({ format: 'cjs' }).code;
+      
+      // don't look at me. this is how Rollup does it.
+      var defaultLoader = require.extensions['.js'];
+      
+      require.extensions['.js'] = function(m, filename) {
+        if(filename === options) {
+          m._compile(code, filename);
+        } else {
+          defaultLoader(m, filename);
+        }
+      };
+      
+      try {
+        return require(options);
+      } finally {
+        require.extensions['.js'] = defaultLoader;
+      }
+    });
   }
   
-  rollup.rollup(options).then(function(bundle) {
-    bundle = bundle.generate(options);
-    var code = bundle.code, map = bundle.map;
-    
-    stream.push(code);
-    if(options.sourceMap) {
-      stream.push('\n//# sourceMappingURL=');
-      stream.push(map.toUrl());
+  options.then(function(options) {
+    var rollup = (options && options.rollup) || require('rollup');
+    if(options && 'rollup' in options) {
+      delete options.rollup;
     }
-    stream.push(null);
+    
+    return rollup.rollup(options).then(function(bundle) {
+      bundle = bundle.generate(options);
+      var code = bundle.code, map = bundle.map;
+      
+      stream.push(code);
+      if(options.sourceMap) {
+        stream.push('\n//# sourceMappingURL=');
+        stream.push(map.toUrl());
+      }
+      stream.push(null);
+    });
   }).catch(function(reason) {
     setImmediate(function() {
       stream.emit('error', reason);
